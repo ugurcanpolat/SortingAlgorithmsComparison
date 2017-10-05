5c1ff0481bb4735fa9281041eede38efb9b60a3d
Uğurcan Polat <ugurcanpolat@Ugurcans-MacBook-Pro.local>
2017-10-05 20:04:53 +0300

    Initial commit

---
+++ algorithm-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm-1
+//
+//  Created by Uğurcan Polat on 5.10.2017.
+//  Copyright © 2017 Uğurcan Polat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
